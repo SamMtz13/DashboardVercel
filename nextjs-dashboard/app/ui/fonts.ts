484e960bd0f8620d2f9aedea9aaf7bb64cdd07f1
Alan Samuel Martinez Alonso <leumas.mart26@gmail.com>
2024-10-13 22:43:23 -0600

    feat: adds Lusitana font

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter } from "next/font/google";
+import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] }); // Se importa la fuente Inter de Google Fonts con el subconjunto latin para soportar caracteres especiales en español
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
