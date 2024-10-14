ac75e1c444e70f9b1510e2f3dd55f83b8d80b858
Alan Samuel Martinez Alonso <leumas.mart26@gmail.com>
2024-10-13 22:37:15 -0600

    feat: adds inter font

---
+++ nextjs-dashboard/app/ui/font.ts
@@ -0,0 +1,3 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] }); // Se importa la fuente Inter de Google Fonts con el subconjunto latin para soportar caracteres especiales en español
