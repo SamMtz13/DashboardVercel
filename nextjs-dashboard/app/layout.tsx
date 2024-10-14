e875e8c64eb3625d896f4b16de9e8d6feb399104
Alan Samuel Martinez Alonso <leumas.mart26@gmail.com>
2024-10-13 22:50:08 -0600

    feat: add lusitana to app

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={"${inter.classname} antialiased"}>{children}</body>
+      <body className={" ${inter.classname} antialiased"}>{children}</body>
     </html>
   );
 }
